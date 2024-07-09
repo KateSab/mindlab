f732bb3cff5524e0be8c34de213f778805f40212
Kate <greyshan@student.21-school.ru>
2024-07-09 15:19:39 +0700

    create store

---
+++ src/store/index.ts
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import axios from 'axios';
+import { IPost } from '../interfaces/IPost';
+
+export const useBlogStore = defineStore('blogStore', () => {
+  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+  const posts = ref<IPost[]>([]);
+  const current_post = ref<IPost>({} as IPost);
+
+  async function get_all_posts() {
+    try {
+      const response = await axios.get(`${BASE_URL}` + '/posts');
+      const data = await response.data;
+      posts.value = data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function get_post(id: number) {
+    try {
+      const response = await fetch(`${BASE_URL}` + `/posts?id=${id}`);
+      const data = await response.json();
+      current_post.value.userId = data.userId;
+      current_post.value.id = data.id;
+      current_post.value.title = data.title;
+      current_post.value.body = data.body;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return { posts, get_all_posts, get_post }
+})
