ed0eb2c674733de5f2dd4940cd9cc69565b7bd43
Kate <greyshan@student.21-school.ru>
2024-07-09 15:21:06 +0700

    create home page

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title">
+    <h1>Лучше, чем twitter</h1>
+  </div>
+  <div class="posts">
+    <div>
+      <PostItem v-for="post in postsStore.posts" :key="post.id" :post="post" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useBlogStore } from '../store/index'
+import PostItem from '../components/PostItem.vue'
+
+const postsStore = useBlogStore();
+
+onMounted(() => {
+  postsStore.get_all_posts();
+
+  console.log(postsStore.posts);
+})
+</script>
