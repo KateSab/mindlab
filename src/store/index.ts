5c24869079999e744eaa5508d157611ef85e8454
Kate <greyshan@student.21-school.ru>
2024-07-10 20:52:26 +0700

    delete not usable code

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,26 +6,12 @@ import { IPost } from '../interfaces/IPost';
 export const useBlogStore = defineStore('blogStore', () => {
   const BASE_URL = 'https://jsonplaceholder.typicode.com';
   const posts = ref<IPost[]>([]);
-  const current_post = ref<IPost>({} as IPost);
 
   async function get_all_posts() {
     try {
       const response = await fetch(`${BASE_URL}` + '/posts');
       const data = await response.json();
       posts.value = data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async function get_post(id: number) {
-    try {
-      const response = await fetch(`${BASE_URL}` + `/posts?id=${id}`);
-      const data = await response.json();
-      current_post.value.userId = data.userId;
-      current_post.value.id = data.id;
-      current_post.value.title = data.title;
-      current_post.value.body = data.body;
     } catch (error) {
       console.error(error);
     }
@@ -64,5 +50,5 @@ export const useBlogStore = defineStore('blogStore', () => {
     }
   }
 
-  return { posts, get_all_posts, get_post, add_post, delete_post }
+  return { posts, get_all_posts, add_post, delete_post }
 })
